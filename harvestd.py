32355a0f746a6f23702d67e166de91bb77b4d298
Mike Kazantsev <mk.fraggod@gmail.com>
2011-12-24 20:47:08 +0600

    harvestd: dropped dev_resolve function

--- harvestd.py
+++ harvestd.py
@@ -243,28 +243,6 @@ def file_follow_durable( path,
 			break
 
 
-def dev_resolve(major, minor, _cache = dict(), _cache_time=610):
-	ts_now = time()
-	while True:
-		if not _cache: ts = 0
-		else:
-			dev = major, minor
-			dev_cached, ts = (None, _cache[None])\
-				if dev not in _cache else _cache[dev]
-		# Update cache, if necessary
-		if ts_now > ts + _cache_time:
-			_cache.clear()
-			for link in it.chain(iglob('/dev/mapper/*'), iglob('/dev/sd*')):
-				link_name = os.path.basename(link)
-				try: link_dev = os.stat(link).st_rdev
-				except OSError: continue # EPERM, EINVAL
-				_cache[(os.major(link_dev), os.minor(link_dev))] = link_name, ts_now
-			_cache[None] = ts_now
-			continue # ...and try again
-		if dev_cached: dev_cached = dev_cached.replace('.', '_')
-		return dev_cached
-
-
 
 class Collector(object): pass
 
