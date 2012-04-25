81f545528819e65ae4c974d913b48a6c630c6ed3
Mike Kazantsev <mk.fraggod@gmail.com>
2012-04-26 00:37:37 +0600

    sinks.librato_metrics: fixed auth processing and content-type passing

--- graphite_metrics/sinks/librato_metrics.py
+++ graphite_metrics/sinks/librato_metrics.py
@@ -27,13 +27,12 @@ class LibratoMetrics(Sink):
 				from . import cfg
 				self.conf.http_parameters.timeout = cfg.loop.interval / 2
 			except (ImportError, KeyError): self.conf.http_parameters.timeout = 30
+		if isinstance(self.conf.http_parameters.auth, list):
+			self.conf.http_parameters.auth = tuple(self.conf.http_parameters.auth)
 
 		requests.defaults.danger_mode = True
 		requests.defaults.keep_alive = True
 		requests.defaults.max_retries = max(3, self.conf.http_parameters.timeout / 5)
-		requests.defaults.base_headers = {'content-type': 'application/json'}
-
-
 
 	def measurement(self, name, value, ts_dp=None):
 		measurement = dict()
@@ -50,8 +49,8 @@ class LibratoMetrics(Sink):
 			data['measure_time'] = int(time())
 			tuples = ((name, value, None) for name, value, ts_dp in tuples)
 		data['gauges'] = list(it.starmap(self.measurement, tuples))
-		requests.post(
-			data=dumps(data), **self.conf.http_parameters )
+		requests.post( data=dumps(data),
+			headers={'content-type': 'application/json'}, **self.conf.http_parameters )
 
 
 sink = LibratoMetrics
