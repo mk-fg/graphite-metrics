mk-fg/graphite-metrics

commit 1de86905eac31862833da44ea9b16da7622cdc03

sinks.carbon_socket: iterate over whatever getaddrinfo() returns in proper order

graphite_metrics/sinks/carbon_socket.py
@@ -25,8 +25,8 @@ class CarbonSocket(Sink):
 		while True:
 			try:
 				try:
-					addrinfo = list(socket.getaddrinfo(
-						host, port, socket.AF_UNSPEC, socket.SOCK_STREAM ))
+					addrinfo = reversed(list(socket.getaddrinfo(
+						host, port, socket.AF_UNSPEC, socket.SOCK_STREAM )))
 				except socket.error as err:
 					raise socket.gaierror(err.message)
 				assert addrinfo, addrinfo

setup.py
@@ -24,7 +24,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'graphite-metrics',
-	version = '13.08.2',
+	version = '13.08.3',
 	author = 'Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'WTFPL',

commit 46504f0081aa30df95603c75f675b506cb1e8316

sinks.carbon_socket: fix for breakage in 1de8690

graphite_metrics/sinks/carbon_socket.py
@@ -25,7 +25,7 @@ class CarbonSocket(Sink):
 		while True:
 			try:
 				try:
-					addrinfo = reversed(list(socket.getaddrinfo(
+					addrinfo = list(reversed(socket.getaddrinfo(
 						host, port, socket.AF_UNSPEC, socket.SOCK_STREAM )))
 				except socket.error as err:
 					raise socket.gaierror(err.message)

setup.py
@@ -24,7 +24,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'graphite-metrics',
-	version = '13.08.4',
+	version = '13.08.5',
 	author = 'Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'WTFPL',