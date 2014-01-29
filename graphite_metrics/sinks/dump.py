d4ebecd74bd65b978f29228607bce7a10ce74acd
Mike Kazantsev <mk.fraggod@gmail.com>
2014-01-29 17:36:28 +0600

    sinks.dump: add dump start/end lines, config example

--- graphite_metrics/sinks/dump.py
+++ graphite_metrics/sinks/dump.py
@@ -13,8 +13,10 @@ class Dumper(Sink):
 	'Just dumps the data to log. Useful for debugging.'
 
 	def dispatch(self, *tuples):
+		log.info('--- dump of {} datapoints'.format(len(tuples)))
 		for name, value, ts_dp in tuples:
 			log.info('Datapoint: {} {} {}'.format(name, value, ts_dp))
+		log.info('--- dump end')
 
 
 sink = Dumper
